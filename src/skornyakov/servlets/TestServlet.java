8ad483f55309edc7052e2707aba4c0a854de3229
Alexander Skornyakov <skornyakov@outlook.com>
2014-06-07 07:43:48 +0400

    Servlet Added

---
+++ src/skornyakov/servlets/TestServlet.java
@@ -0,0 +1,20 @@
+package skornyakov.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by A on 07.06.2014.
+ */
+public class TestServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
