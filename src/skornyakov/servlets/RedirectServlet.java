a1e3feb746458b3a8eea1443b34cc650b605701f
sk1212 <skornyakov@usa.com>
2014-06-11 15:36:19 +0400

    Added redirect to google.com

--- src/skornyakov/servlets/RedirectServlet.java
+++ src/skornyakov/servlets/RedirectServlet.java
@@ -1,6 +1,7 @@
 package skornyakov.servlets;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class RedirectServlet
  */
-@WebServlet("/RedirectServlet")
+@WebServlet("/redirect")
 public class RedirectServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
@@ -28,7 +29,8 @@ public class RedirectServlet extends HttpServlet {
      */
     protected void doGet(HttpServletRequest request,
 	    HttpServletResponse response) throws ServletException, IOException {
-	// TODO Auto-generated method stub
+	String location = "http://google.com";
+	response.sendRedirect(location);
     }
 
     /**
