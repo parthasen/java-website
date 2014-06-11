3866e090f6a1ce60dfcce0b72c9a561aa90a80af
sk1212 <skornyakov@usa.com>
2014-06-11 15:07:07 +0400

    Post reffered by get

--- src/skornyakov/servlets/DateServlet.java
+++ src/skornyakov/servlets/DateServlet.java
@@ -28,7 +28,7 @@ public class DateServlet extends HttpServlet {
      */
     protected void doGet(HttpServletRequest request,
 	    HttpServletResponse response) throws ServletException, IOException {
-	// TODO Auto-generated method stub
+	
     }
 
     /**
@@ -37,7 +37,7 @@ public class DateServlet extends HttpServlet {
      */
     protected void doPost(HttpServletRequest request,
 	    HttpServletResponse response) throws ServletException, IOException {
-	// TODO Auto-generated method stub
+	this.doGet(request,response);
     }
 
 }
