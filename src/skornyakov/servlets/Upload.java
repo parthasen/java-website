c9701995dd214985dfe74fc1be5cb1e12921c992
Alexander <skornyakov@outlook.com>
2014-06-13 13:12:14 +0400

    initial commit

--- src/skornyakov/servlets/Upload.java
+++ src/skornyakov/servlets/Upload.java
@@ -19,54 +19,55 @@ import javax.servlet.http.Part;
  */
 @MultipartConfig
 public class Upload extends HttpServlet {
-    /**
+	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 
 	protected void doPost(HttpServletRequest request,
-	    HttpServletResponse response) throws ServletException, IOException {
-	String destination = System.getProperty("user.home");
-	Part filePart = request.getPart("file");
-	String fileName = getFileName(filePart);
-	InputStream fileInputStream = null;
-	try {
-	    fileInputStream = filePart.getInputStream();
-	    File file = new File(destination + File.separator + fileName);
-	    if (!file.exists()) {
-		file.createNewFile();
-	    }
-	    OutputStream fileOutputStream = new FileOutputStream(file);
-	    int read = 0;
-	    byte[] bytes = new byte[1024];
-	    while ((read = fileInputStream.read(bytes)) != -1) {
-		fileOutputStream.write(bytes, 0, read);
-	    }
-	    fileOutputStream.close();
-	    fileInputStream.close();
-	} catch (IOException e) {
-	    e.printStackTrace();
+			HttpServletResponse response) throws ServletException, IOException {
+		String destination = System.getProperty("user.home");
+		Part filePart = request.getPart("file");
+		String fileName = getFileName(filePart);
+		InputStream fileInputStream = null;
+		try {
+			fileInputStream = filePart.getInputStream();
+			File file = new File(destination + File.separator + fileName);
+			if (!file.exists()) {
+				file.createNewFile();
+			}
+			OutputStream fileOutputStream = new FileOutputStream(file);
+			int read = 0;
+			byte[] bytes = new byte[1024];
+			while ((read = fileInputStream.read(bytes)) != -1) {
+				fileOutputStream.write(bytes, 0, read);
+			}
+			fileOutputStream.close();
+			fileInputStream.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		FileWriter fw = new FileWriter(destination + File.separator
+				+ "request.txt");
+		fw.write(request.getParts().toString());
+		fw.close();
+		String location = this.getServletContext().getContextPath()
+				+ "/index.jsp";
+		response.sendRedirect(location);
 	}
-	FileWriter fw = new FileWriter(destination+File.separator+"request.txt");
-	fw.write(request.getParts().toString());
-	fw.close();
-	String location = this.getServletContext().getContextPath()
-		+ "/index.jsp";
-	response.sendRedirect(location);
-    }
 
-    protected void doGet(HttpServletRequest request,
-	    HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 
-    }
-
-    public String getFileName(Part part) {
-	for (String content : part.getHeader("content-disposition").split(";")) {
-	    if (content.trim().startsWith("filename")) {
-		return content.substring(content.indexOf('=') + 1).trim()
-			.replace("\"", "");
-	    }
 	}
-	return null;
-    }
+
+	public String getFileName(Part part) {
+		for (String content : part.getHeader("content-disposition").split(";")) {
+			if (content.trim().startsWith("filename")) {
+				return content.substring(content.indexOf('=') + 1).trim()
+						.replace("\"", "");
+			}
+		}
+		return null;
+	}
 }
