dda4de0cc5edd9c84a56dce7bca62e34160f6b80
Alexander <skornyakov@outlook.com>
2014-06-13 13:13:38 +0400

    second commit

--- src/skornyakov/servlets/Upload.java
+++ src/skornyakov/servlets/Upload.java
@@ -19,9 +19,7 @@ import javax.servlet.http.Part;
  */
 @MultipartConfig
 public class Upload extends HttpServlet {
-	/**
-	 * 
-	 */
+	
 	private static final long serialVersionUID = 1L;
 
 	protected void doPost(HttpServletRequest request,
