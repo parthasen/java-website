a7a34dc3346f41ed31b2e8d5f49aa1e5759b326d
sk1212 <skornyakov@usa.com>
2014-06-15 14:15:31 +0400

    Lazy bean added

---
+++ src/skornyakov/beans/spring/LazyBean.java
@@ -0,0 +1,14 @@
+package skornyakov.beans.spring;
+
+public class LazyBean {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+    
+}
--- src/skornyakov/servlets/TestServlet.java
+++ src/skornyakov/servlets/TestServlet.java
@@ -1,18 +1,17 @@
 package skornyakov.servlets;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.web.context.support.XmlWebApplicationContext;
 
 import skornyakov.beans.spring.HelloMessage;
-
-import java.io.IOException;
-import java.io.PrintWriter;
+import skornyakov.beans.spring.LazyBean;
 
 /**
  * Created by A on 07.06.2014.
@@ -21,25 +20,31 @@ public class TestServlet extends HttpServlet {
     /**
 	 * 
 	 */
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request,
+	    HttpServletResponse response) throws ServletException, IOException {
 
     }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        PrintWriter pw = response.getWriter();
-        
-        XmlWebApplicationContext context = new XmlWebApplicationContext ();
-        context.setConfigLocation("/WEB-INF/Beans.xml");
-        context.setServletContext(this.getServletContext());
-        context.refresh();
-        String beanName = "helloMessage";
-	HelloMessage hm = (HelloMessage)context.getBean(beanName);
+    protected void doGet(HttpServletRequest request,
+	    HttpServletResponse response) throws ServletException, IOException {
+	PrintWriter pw = response.getWriter();
+
+	XmlWebApplicationContext context = new XmlWebApplicationContext();
+	context.setConfigLocation("/WEB-INF/Beans.xml");
+	context.setServletContext(this.getServletContext());
+	context.refresh();
+	String beanName = "helloMessage";
+	HelloMessage hm = (HelloMessage) context.getBean(beanName);
 	String message = hm.getMessage();
-        pw.println("<html><head><title>Test</title></head><body><h1>Test Servlet</h1><h2>"
-        	+ message +
-        	"</h2></body></html>");
-        pw.close();
+	beanName = "lazyBeanExample";
+	LazyBean lb = (LazyBean)context.getBean(beanName);
+	String lazyMessage = lb.getMessage();
+	pw.println("<html><head><title>Test</title></head><body><h1>Test Servlet</h1><h2>"
+		+ message + "</h2>"
+			+ "<h2>" + lazyMessage + "</h2>"
+			+ "</body></html>");
+	pw.close();
     }
 }
