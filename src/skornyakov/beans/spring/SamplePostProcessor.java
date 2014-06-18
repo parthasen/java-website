5626a2d5c6617f58e0d455596bef723f6ac47e60
sk1212 <skornyakov@usa.com>
2014-06-18 09:52:48 +0400

    changes

--- src/skornyakov/beans/spring/SamplePostProcessor.java
+++ src/skornyakov/beans/spring/SamplePostProcessor.java
@@ -7,15 +7,17 @@ import org.springframework.beans.factory.config.BeanPostProcessor;
  * Created by Alexander on 16.06.2014.
  */
 public class SamplePostProcessor implements BeanPostProcessor {
-    @Override
-    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
-        System.out.println("Before bean initialization");
-        return bean;
-    }
+	@Override
+	public Object postProcessBeforeInitialization(Object bean, String beanName)
+			throws BeansException {
+		System.out.println("Before bean initialization");
+		return bean;
+	}
 
-    @Override
-    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
-        System.out.println("After bean initialization");
-        return bean;
-    }
+	@Override
+	public Object postProcessAfterInitialization(Object bean, String beanName)
+			throws BeansException {
+		System.out.println("After bean initialization");
+		return bean;
+	}
 }
